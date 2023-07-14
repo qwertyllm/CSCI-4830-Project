d781f43f5623f28a784aa2d1aef2bd234e862694
jacob-finley <jacobfinley@unomaha.edu>
2023-07-13 19:51:21 -0500

    Adding Jacob's additions. The editions being: questions.js file which grabs the data from questions.json; quiz.js file was updated to accommodate with the changes made from questions.js

--- src/quiz.js
+++ src/quiz.js
@@ -1,6 +1,4 @@
 // this file contains core backend functionality
-
-
 import React from 'react';
 import questions from './questions';
 
@@ -8,28 +6,75 @@ function Quiz() {
     const [currentQuestion, setCurrentQuestion] = React.useState(0);
     const [showScore, setShowScore] = React.useState(false);
     // constants for 'points' of different college
-    const [scores, setScores] = React.useState({
+    const [scores, updateScores] = React.useState({
         collegeARTSCI: 0,
         collegeBA: 0,
         collegeEDU: 0,
         collegeCOM: 0,
         collegeSCITECH: 0,
-        collegePUBLIC: 0,
-    });
+        collegePUBLIC: 0
+    })
 
     // reads button presses and updates scores as needed.
-    const handleAnswerButtonClick = (scores) => {
-        setScores((prevScores) => ({
+    const handleAnswerButtonClick = (scores, index) => {
+        if (index === 0) {
+            updateScores((prevScores) => ({
+                collegeARTSCI: prevScores.collegeARTSCI + scores[0],
+                collegeBA: prevScores.collegeBA,
+                collegeEDU: prevScores.collegeEDU,
+                collegeCOM: prevScores.collegeCOM,
+                collegeSCITECH: prevScores.collegeSCITECH,
+                collegePUBLIC: prevScores.collegePUBLIC
+            }));
+        } else if (index === 1) {
+            updateScores((prevScores) => ({
+                collegeARTSCI: prevScores.collegeARTSCI,
+                collegeBA: prevScores.collegeBA  + scores[1],
+                collegeEDU: prevScores.collegeEDU,
+                collegeCOM: prevScores.collegeCOM,
+                collegeSCITECH: prevScores.collegeSCITECH,
+                collegePUBLIC: prevScores.collegePUBLIC
+            }));
+        } else if (index === 2) {
+            updateScores((prevScores) => ({
+                collegeARTSCI: prevScores.collegeARTSCI,
+                collegeBA: prevScores.collegeBA,
+                collegeEDU: prevScores.collegeEDU + scores[2],
+                collegeCOM: prevScores.collegeCOM,
+                collegeSCITECH: prevScores.collegeSCITECH,
+                collegePUBLIC: prevScores.collegePUBLIC
+            }));
+        } else if (index === 3) {
+            updateScores((prevScores) => ({
+                collegeARTSCI: prevScores.collegeARTSCI,
+                collegeBA: prevScores.collegeBA,
+                collegeEDU: prevScores.collegeEDU,
+                collegeCOM: prevScores.collegeCOM + scores[3],
+                collegeSCITECH: prevScores.collegeSCITECH,
+                collegePUBLIC: prevScores.collegePUBLIC
+            }));
+        } else if (index === 4) {
+            updateScores((prevScores) => ({
+                collegeARTSCI: prevScores.collegeARTSCI,
+                collegeBA: prevScores.collegeBA,
+                collegeEDU: prevScores.collegeEDU,
+                collegeCOM: prevScores.collegeCOM,
+                collegeSCITECH: prevScores.collegeSCITECH + scores[4],
+                collegePUBLIC: prevScores.collegePUBLIC
+            }));
+        } else if (index === 5) {
+            updateScores((prevScores) => ({
+                collegeARTSCI: prevScores.collegeARTSCI,
+                collegeBA: prevScores.collegeBA,
+                collegeEDU: prevScores.collegeEDU,
+                collegeCOM: prevScores.collegeCOM,
+                collegeSCITECH: prevScores.collegeSCITECH,
+                collegePUBLIC: prevScores.collegePUBLIC  + scores[5]
+            }));
+        }
 
-            collegeARTSCI: prevScores.collegeARTSCI + scores.collegeARTSCI,
-            collegeBA: prevScores.collegeBA + scores.collegeBA,
-            collegeEDU: prevScores.collegeEDU + scores.collegeEDU,
-            collegeCOM: prevScores.collegeCOM + scores.collegeCOM,
-            collegeSCITECH: prevScores.collegeSCITECH + scores.collegeSCITECH,
-            collegePUBLIC: prevScores.collegePUBLIC + scores.collegePUBLIC,
 
 
-        }));
         // advances the question if there are more questions in question.js to go through
         const nextQuestion = currentQuestion + 1;
         if (nextQuestion < questions.length) {
@@ -71,17 +116,17 @@ function Quiz() {
                 // handles displaying buttons and the current question. 
                 // pulls questions from questions.js
                 <div className="question-card">
-                    <h2><div className="question-number">Question {currentQuestion + 1} of  {questions.length}</div></h2>
+                    <h2><div className="question-number">Question {currentQuestion + 1} of {questions.length}</div></h2>
                     <h2>{questions[currentQuestion].questionText}</h2>
                     <div className="answer-container">
-                    {questions[currentQuestion].answerOptions.map((answerOption, index) => (
-                        <button
-                            key={index}
-                            onClick={() => handleAnswerButtonClick(answerOption.scores)}
-                        >
-                            {answerOption.answerText}
-                        </button>
-                    ))}
+                        {questions[currentQuestion].answerOptions?.map((answerOption, index) => (
+                            <button
+                                key={index}
+                                onClick={() => handleAnswerButtonClick(questions[currentQuestion].scores, index)}
+                            >
+                                {answerOption}
+                            </button>
+                        ))}
                     </div>
                 </div>
             )}
